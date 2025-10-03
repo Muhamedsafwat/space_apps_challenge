b772aebc6da25fa556e51f6ab266939e6ba860b4
Muhamedsafwat <musafwat666@gmail.com>
2025-10-03 22:43:00 +0300

    add compare section

--- src/app/_components/Sample.jsx
+++ src/app/_components/Sample.jsx
@@ -18,8 +18,8 @@ const Sample = () => {
       <div className="w-full aspect-video overflow-hidden relative">
         <div className="top-1/2 -translate-y-1/2 absolute w-full aspect-video">
           <ReactCompareImage
-            leftImage="https://lh3.googleusercontent.com/aida-public/AB6AXuBvV0IrWm4UWZgXX8VOxO8mshK1XeYs6XeNQi1JrA0t64d80oq9chJDU18__XdYLg13G-rrAODYV_LmBpbn3cwtaqo0QD4VhIbejmYk6-tfvpDTXJfkhAsMjFjCEGDsOYhUYcqngSoSjAvnsD_UL2pXvXdYHDV0JOp-5S8E3J4Uu75230Pu8RmWuOwFUtZo_9IN68KX1-r1E1vEwLyeuKoYTz02td7ANBJGEuxFRBqZw08Iubso30pfD8mnwN0Nnk8sFEjWhWUNqq4"
-            rightImage="https://lh3.googleusercontent.com/aida-public/AB6AXuA2j1nk3i7UaSaeB4keeuUVljyfxsCcIFswnl_1psg8lZYghU0XSbkTw7-5UPwNXLklpWwqb5fOuCDkw7S_fa4SPGDO2IL1vBDKYUpzZ0tN9Z8Xv-KlfD3Fp-j2mUvjYedqdo7uV4p9US-ZGD37VFneE1oObQGYtj446CLABijYG0U10hBacyYv-rKou0whyFYzOP2CSCfAzOxKXpGc4Br14zXKWctGNWaFiOjRwPGNcSr-LQ4nJfglmc3qyZbiyEJgShyCDBnJHug"
+            leftImage="/images/earth1.png"
+            rightImage="/images/nisar1.png"
           />
         </div>
       </div>
